d85591f89d2bbdb066a136e39b11020e363d294b
kenembanisi <kene.mbanisi@gmail.com>
2025-04-14 07:15:31 -0400

    update camera interface script

--- scripts/camera_display.py
+++ scripts/camera_display.py
@@ -1,34 +1,12 @@
 import cv2
 
-cap = cv2.VideoCapture(4)
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
 
-    cv2.imshow('Camera frame', frame)
-    if cv2.waitKey(1) == ord('q'):
-        cv2.imwrite("frame.png", frame)  # Save as PNG
-        break
-
+if ret:
+    print("Saving to frame.png")
+    cv2.imwrite("frame.png", frame)
+else:
+    print("Failed to capture frame")
 
 cap.release()
-cv2.destroyAllWindows()
-
-
-# Use to check to find where the usb camera is located
-# import cv2
-
-# available_cameras = []
-# for device_id in range(5):
-#     cap = cv2.VideoCapture(device_id)
-#     if cap.isOpened():
-#         print(f"Found camera at index {device_id}")
-#         available_cameras.append(device_id)
-#         cap.release()
-#     else:
-#         print(f"No camera at index {device_id}")
-
-
-
-
